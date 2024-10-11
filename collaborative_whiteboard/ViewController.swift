4855c034c9c9774d7d19b709023a2450526ca5ee
Rutuja Hulsure <hulsurerutuja22@gmail.com>
2024-10-12 00:04:10 +0530

    Initial Commit

---
+++ collaborative_whiteboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collaborative_whiteboard
+//
+//  Created by Pushkar Ghogare on 12/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
